ad7a26c62e4f530d3fa563e1702cde8f73fbc948
Milkey Tan <819774989@qq.com>
2017-03-27 11:05:10 +0800

    判断

--- app/src/main/java/cc/milione/me/mdict/MainActivity.java
+++ app/src/main/java/cc/milione/me/mdict/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -20,11 +21,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextWord;
+    TextView tViewWord;
+    TextView tViewBrep;
+    TextView tViewPos1;
+    TextView tViewPos2;
+    TextView tViewMn1;
+    TextView tViewMn2;
     String bingDictPath = "http://xtk.azurewebsites.net/BingService.aspx";
 
     @Override
@@ -33,32 +41,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextWord = (EditText) findViewById(R.id.editTextWord);
+        tViewWord = (TextView) findViewById(R.id.textViewWord);
+        tViewBrep = (TextView) findViewById(R.id.textViewBrep);
+        tViewPos1 = (TextView) findViewById(R.id.textViewPos1);
+        tViewPos2 = (TextView) findViewById(R.id.textViewPos2);
+        tViewMn1 = (TextView) findViewById(R.id.textVieMn1);
+        tViewMn2 = (TextView) findViewById(R.id.textVieMn2);
     }
 
     public void searchOnClick(View view) {
         String wordVal = "Action=search&Format=jsonwv&Word=" + editTextWord.getText().toString();
         String wordExplain = postWeb(bingDictPath,wordVal);
-        if (wordExplain == null || wordExplain.equals(null) || wordExplain == "" || wordExplain.equals("")) {
-            Toast.makeText(this, "Error:Not Found Explain.", Toast.LENGTH_SHORT).show();
+        if (wordExplain == null || wordExplain.equals("")) {
+            Toast.makeText(this, "mDict未查询到相关内容，请检查", Toast.LENGTH_SHORT).show();
         }
         else {
-            Toast.makeText(this, wordExplain, Toast.LENGTH_SHORT).show();
+            Log.i("mDict","post:" + wordExplain);
+            String mn1 = praseJson(wordExplain,"mn1");
+            if (mn1.equals("")||mn1.equals(" ")||mn1.equals(", ")||mn1.equals("; "))
+            {
+                Toast.makeText(this, "mDict未查询到相关内容，请检查", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                tViewWord.setText(praseJson(wordExplain, "word"));
+                tViewBrep.setText(praseJson(wordExplain, "brep"));
+                tViewPos1.setText(praseJson(wordExplain, "pos1"));
+                tViewPos2.setText(praseJson(wordExplain, "pos2"));
+                tViewMn1.setText(praseJson(wordExplain, "mn1"));
+                tViewMn2.setText(praseJson(wordExplain, "mn2"));
+            }
         }
     }
 
     public String praseJson(String JsonStr, String JsonData) {
         String getData = null;
         try {
-            JSONArray jsonArray = new JSONArray(JsonStr);
+            JSONArray jsonArray = new JSONArray("["+JsonStr+"]");
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject JSONObject = jsonArray.getJSONObject(i);
                 getData = JSONObject.getString(JsonData);
-                Log.d("MainActivity","Get:" + getData);
+                Log.d("mDict","prase:" + getData + "val:" + JsonData);
             }
         }
         catch (JSONException e) {
             e.printStackTrace();
-            getData = "Error";
+            getData = "";
         }
         return getData;
     }
@@ -87,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
                         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                         String linestr;
                         while ((linestr = bufferedReader.readLine()) != null){
-                            Log.d("MainActivity",linestr);
+                            Log.d("mDict","Json:" + linestr);
                             line = linestr;
                         }
                         bufferedReader.close();
